a3baf4bdc68cad4846749d24274fb1a8684bb4fc
Debankur Saha <debankurrocks@gmail.com>
2019-08-24 15:07:38 +0530

    Changed output Directory

--- tfrecgen.sh
+++ tfrecgen.sh
@@ -1,5 +1,5 @@
-mkdir tfrec
-python generate_tfrecord.py --csv_input=images/train/train_labels.csv  --output_path=tfrec/train.record --image_dir=images/train
+mkdir data
+python generate_tfrecord.py --csv_input=images/train/train_labels.csv  --output_path=data/train.record --image_dir=images/train
 
 # Create test data:
-python generate_tfrecord.py --csv_input=images/test/test_labels.csv  --output_path=tfrec/test.record --image_dir=images/test
+python generate_tfrecord.py --csv_input=images/test/test_labels.csv  --output_path=data/test.record --image_dir=images/test
